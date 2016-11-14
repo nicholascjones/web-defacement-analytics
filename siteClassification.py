c134a3fd82ca059fb82e2a6428f5e015e4443c34
Nicholas Jones <njones7@nd.edu>
2016-11-14 16:37:22 -0500

    blah

--- siteClassification.py
+++ siteClassification.py
@@ -6,16 +6,12 @@ with open("./blacklists/ads/urls")as f:
 	uL = f.readlines()
 
 
-print uL
-
-uL = set(uL)
 
 for s in uL:
 	p = urlparse(s)
 	if p.netloc:
-		print p.netloc
-	else:
-		print "~~~~~~~~\n~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~"
+		pass
+		#print p.netloc
 
 
 urlList = []
@@ -25,9 +21,24 @@ cats = []
 for x,y,z in os.walk("./blacklists"):
 
 	#print x,y,z  ## only want x
-	cats.append(x)
+	cats.append(x) #categories
 
-print cats
+
+categories = dict((c,None) for c in cats)
+
+print categories
+
+"""
+for c in cats:
+	fs = "./blacklists/" + str(c) + "domains"
+	with open(fs) as domainList:
+		doms = domainList.readlines()
+"""
+
+
+
+
+
 
 
 
