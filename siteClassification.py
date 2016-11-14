b1f8fea25a6f3d31f8ee1457c1ddd59fd6ae0bdc
Nicholas Jones <njones7@nd.edu>
2016-11-14 16:50:04 -0500

    added key sets

--- siteClassification.py
+++ siteClassification.py
@@ -5,35 +5,41 @@ from urlparse import urlparse
 with open("./blacklists/ads/urls")as f:
 	uL = f.readlines()
 
+urlList = []
 
 
 for s in uL:
 	p = urlparse(s)
 	if p.netloc:
-		pass
-		#print p.netloc
+		urlList.append(p.netloc)
 
 
-urlList = []
 
 cats = []
 
 for x,y,z in os.walk("./blacklists"):
 
-	#print x,y,z  ## only want x
-	cats.append(x) #categories
+	print x,y,z  ## only want x
+	q = x.replace("./blacklists/","")
+	#print q
+	cats.append(q) #categories
 
 
 categories = dict((c,None) for c in cats)
 
 print categories
 
-"""
-for c in cats:
-	fs = "./blacklists/" + str(c) + "domains"
-	with open(fs) as domainList:
-		doms = domainList.readlines()
-"""
+
+for key,value in categories.iteritems():
+	try:
+		fs = "./blacklists/" + str(key) + "/domains"
+		with open(fs) as domainList:
+			doms = domainList.read().splitlines()
+		categories[key] = set(doms)
+	except:
+		pass
+
+urlTest = {}
 
 
 
