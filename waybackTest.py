ecc6b09bd41180b3f5f5e21a18b3c22821e28330
Nicholas Jones <njones7@nd.edu>
2016-11-21 17:41:51 -0500

    added the all Good list to see what works

--- waybackTest.py
+++ waybackTest.py
@@ -7,31 +7,38 @@ urlList = []
 with open('output.csv', 'rb') as cf:
 	rdr = csv.reader(cf, delimiter=',')
 	for row in rdr:
-		urlList.append(row[6])
+		if row[6] != 'Domain':
+			urlList.append(row[6])
 
 #print urlList
 
+allGood = []
 incompleteDomains = []
 noArchive = []
 
 for u in urlList:
 	
 	if u[-3:] == "...":
-		print "flag"
+		#print "flag"
 		incompleteDomains.append(u)
 	else:
 		uStr = "http://archive.org/wayback/available?url=" + u
-		print uStr
+		#print uStr
 		z = requests.get(uStr)
 		q = z.json()
-		print q
+		#print q
 		if not q['archived_snapshots']:
 			noArchive.append(u)
+		else:
+			allGood.append(u)
+
 
 print "incomplete domains:"
 print incompleteDomains
 print "\nno archive:"
 print noArchive
+print "\nall good:"
+print allGood
 	
 	
 	
