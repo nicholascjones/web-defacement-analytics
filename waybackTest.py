51482a08d7b45d49a0d13bb0ea9b55c9da6597eb
Nicholas Jones <njones7@nd.edu>
2016-11-21 14:57:33 -0500

    got row index for csv reader

--- waybackTest.py
+++ waybackTest.py
@@ -1,8 +1,17 @@
 import requests
 import json
+import csv
 
+with open('output.csv', 'rb') as cf:
+	rdr = csv.reader(cf, delimiter=',')
+	for row in rdr:
+		print row[6]
+
+
+"""
 z = requests.get("http://archive.org/wayback/available?url=facebook.com")
 q = z.json()
 
-print q['archived_snapshots']['closest']['available']
+print q['archived_snapshots']['closest']['url']
 
+"""
