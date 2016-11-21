64d0978b46a2948a013f7d87bc8e39b416289dd7
Nicholas Jones <njones7@nd.edu>
2016-11-21 16:29:24 -0500

    returns lists of needed changes

--- waybackTest.py
+++ waybackTest.py
@@ -2,16 +2,36 @@ import requests
 import json
 import csv
 
+urlList = []
+
 with open('output.csv', 'rb') as cf:
 	rdr = csv.reader(cf, delimiter=',')
 	for row in rdr:
-		print row[6]
+		urlList.append(row[6])
 
+#print urlList
 
-"""
-z = requests.get("http://archive.org/wayback/available?url=facebook.com")
-q = z.json()
+incompleteDomains = []
+noArchive = []
 
-print q['archived_snapshots']['closest']['url']
+for u in urlList:
+	
+	if u[-3:] == "...":
+		print "flag"
+		incompleteDomains.append(u)
+	else:
+		uStr = "http://archive.org/wayback/available?url=" + u
+		print uStr
+		z = requests.get(uStr)
+		q = z.json()
+		print q
+		if not q['archived_snapshots']:
+			noArchive.append(u)
 
-"""
+print "incomplete domains:"
+print incompleteDomains
+print "\nno archive:"
+print noArchive
+	
+	
+	
