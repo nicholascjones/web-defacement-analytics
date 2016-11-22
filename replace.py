be8c2f9809336e94508c6584b076dc694e62d2aa
Nicholas Jones <njones7@nd.edu>
2016-11-21 22:38:50 -0500

    fixed replace

--- replace.py
+++ replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Allow inline plotting of figures.
-%matplotlib inline
+#%matplotlib inline
 
 # Import useful libraries.
 import matplotlib.pyplot as plt
@@ -10,17 +10,19 @@ import pandas as pd
 from sklearn import preprocessing
 
 # Read in the file URL.
-fileURL = 'https://raw.githubusercontent.com/cse44648/cse44648/master/datasets/hypothyroid.csv'
+fileURL = './NewClusterSheet.csv'
 
 
 # Read the file into a DataFrame.
 data = pd.read_csv(fileURL)
 
 #Replace nominal data with integers
-data = data.replace(['M','F','f','t','y','n'],[0,1,0,1,0,1])
+data = data.replace(['H','M','R',''],[1,1,1,0])
 
 #replace with median
 cols = data.dtypes
 for key, value in cols.items():
     if key != "Class":
         data[key].fillna(data[key].median(),inplace=True)
+
+print data
