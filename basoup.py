ffe031b71f5bbb34e92f8c602ed05ae80f3da340
irishmann313 <bmann@nd.edu>
2016-11-30 19:40:51 -0500

    Added basoup.py and crawled defaced sites

---
+++ basoup.py
@@ -0,0 +1,25 @@
+#!/usr/local/python
+
+import urllib2
+import sys
+import html2text
+from bs4 import *
+
+f = open('allsites.txt', 'r')
+for line in f:
+	url = line.strip()
+	if url != '0':
+		try:
+			print "Requesting at: " + url
+			response = urllib2.urlopen("http://"+url)
+			#parse html
+			soup = BeautifulSoup(response.read(), "html.parser")
+			filename = './allwebpages/' + url.replace('/', '') + '.txt'
+			fi = open(filename, 'w')
+			text = html2text.html2text(soup.prettify())
+			text = text.encode('ascii', 'ignore')
+			fi.write(text)
+			fi.close()
+		except:
+			print "Error with " + url
+f.close()
